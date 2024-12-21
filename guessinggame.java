20b7a7b9fe9f0a6bb4e2aea8f23b2d09e7851bc5
Nikhil - <127117742+nikv844@users.noreply.github.com>
2024-12-21 16:00:05 +0200

    Update guessinggame.java
    Added loop structure, quit option, and guess counter

--- guessinggame.java
+++ guessinggame.java
@@ -4,12 +4,24 @@ public class guessinggame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String correctName = "Tommi";  // The name to guess
+        int guesses = 0;  // Counter for number of guesses
         
-        System.out.println("Please, guess my name.");
-        String guess = scanner.nextLine();
-        
-        if(guess.equals(correctName)) {
-            System.out.println("Congratulations!");
+        while(true) {
+            System.out.println("Please, guess my name.");
+            String guess = scanner.nextLine();
+            guesses++;
+            
+            if(guess.equals(correctName)) {
+                System.out.println("Congratulations!");
+                System.out.println("Guesses: " + guesses);
+                break;
+            } else {
+                System.out.println("Do you want to quit (y/n) ?");
+                String quit = scanner.nextLine();
+                if(quit.equals("y")) {
+                    break;
+                }
+            }
         }
         
         scanner.close();
